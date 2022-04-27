e9236f604fdfab410425c9629aa59bfc53b62328
aliveGUY <ilyadubrivvin@gmail.com>
2022-04-27 18:55:50 +0300

    Setup To-Do list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,62 @@
-import _ from 'lodash';
 import './style.css';
+import './modules/LinkedList.js';
 
-function component() {
-  const element = document.createElement('div');
+const array = document.querySelector('.List');
+const { LinkedList } = require('./modules/LinkedList.js');
 
-  // Lodash, now imported
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const list = new LinkedList();
 
-  return element;
-}
+const Arr = [
+  {
+    value: 'Task 1',
+    completed: false,
+    index: 0,
+  },
+  {
+    value: 'Task 2',
+    completed: false,
+    index: 1,
+  },
+  {
+    value: 'Task 3',
+    completed: false,
+    index: 2,
+  },
+  {
+    value: 'Task 4',
+    completed: false,
+    index: 3,
+  },
+  {
+    value: 'Task 5',
+    completed: false,
+    index: 4,
+  },
+];
 
-document.body.appendChild(component());
+Arr.forEach((item) => {
+  list.append(item.value, item.completed, item.index);
+  const checked = item.completed ? 'checked' : '';
+  const task = `
+      <li class="list_item" id ="${item.index}">
+          <input name="task${item.index}" type="checkbox" ${checked} class="checkbox">
+          <label for="task${item.index}">${item.value}</label>
+          <button>-</button>
+      </li>
+      <hr/>`;
+  const Child = document.createRange().createContextualFragment(task);
+  array.appendChild(Child);
+});
 
+//  Save checkbox value in Local storage
+const data = JSON.parse(localStorage.getItem('Data')) || [];
+const check = document.querySelectorAll('.checkbox');
+data.forEach((item) => {
+  list.append(item);
+});
+check.forEach((item) => {
+  item.addEventListener('change', () => {
+    data[item.parentNode.id].completed = item.checked;
+    localStorage.setItem('Data', JSON.stringify(data));
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',	
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
